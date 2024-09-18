b96c89e6a7a40590476072c0a84e7229b0ad3682
raphafe <rapha17ferrari@gmail.com>
2024-09-18 19:28:47 -0300

    desafio concluido

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,17 +15,20 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
+
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+
+            if (hospedes.Count() <= Suite.Capacidade)
             {
                 Hospedes = hospedes;
             }
             else
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                throw new Exception($"Não foi possível concluir o cadastro, o nº de hóspedes permitidos é {Suite.Capacidade}!");
             }
+
+
         }
 
         public void CadastrarSuite(Suite suite)
@@ -36,22 +39,25 @@ namespace DesafioProjetoHospedagem.Models
         public int ObterQuantidadeHospedes()
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+
+            return Hospedes.Count();
+
         }
 
         public decimal CalcularValorDiaria()
         {
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+
+            decimal valor = DiasReservados * Suite.ValorDiaria;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                decimal desconto = (10M / 100M) * valor;
+                valor = valor - ((int)desconto);
+
             }
 
             return valor;
